0937e0604cf2b13e46f63148984416b86d9856c5
dev-donghwan <ycdoit@naver.com>
2025-04-19 12:47:54 +0900

    init commit

---
+++ src/test/kotlin/com/donghwan/team_reboott/TeamReboottApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.donghwan.team_reboott
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamReboottApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
